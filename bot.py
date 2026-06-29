7258d206ad3f2a8d1f572e66b9c82f7a20d69e7d
ali-9886 <2401664069@iku.edu.iq>
2026-06-29 21:54:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ threading.Thread(target=run_alive_server, daemon=True).start()
 # =====================================================================
 # ⚙️ إعدادات البوت والاشتراك
 # =====================================================================
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8499856454:AAHB49UPTI7Q4sF0OyMr-GhBPOIVk9aBRRo") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8499856454:AAESPy7NsyzuxTge_UISAZzqeK21ZCPYchs") 
 CHANNELS = ["@iq_2a1"]
 
 bot = telebot.TeleBot(BOT_TOKEN, threaded=True, num_threads=10)
